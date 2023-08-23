6ec568d20ad7acbc71742fa89c556f2f2cbe0df8
Minh Quy <onequy@gmail.com>
2023-08-23 07:31:18 +0700

    fix dl fail

--- scr/handle/CameraRoll.tsx
+++ scr/handle/CameraRoll.tsx
@@ -1,54 +1,15 @@
 import { PermissionsAndroid, Platform } from "react-native";
 import { CameraRoll } from "@react-native-camera-roll/camera-roll";
 
-async function hasAndroidPermission() {
-  const getCheckPermissionPromise = () => {
-    // @ts-ignore
-    if (Platform.Version >= 33) {
-      return Promise.all([
-        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES),
-        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
-      ]).then(
-        ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
-          hasReadMediaImagesPermission && hasReadMediaVideoPermission,
-      );
-    } else {
-      return PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
-    }
-  };
-
-  const hasPermission = await getCheckPermissionPromise();
-  if (hasPermission) {
-    return true;
-  }
-  const getRequestPermissionPromise = () => {
-    // @ts-ignore
-    if (Platform.Version >= 33) {
-      return PermissionsAndroid.requestMultiple([
-        PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
-        PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
-      ]).then(
-        (statuses) =>
-          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
-          PermissionsAndroid.RESULTS.GRANTED &&
-          statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
-          PermissionsAndroid.RESULTS.GRANTED,
-      );
-    } else {
-      return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
-    }
-  };
-
-  return await getRequestPermissionPromise();
-}
-
+/**
+ * 
+ * @returns null if success, otherwise error
+ */
 export async function SaveToGalleryAsync(flp: string) {
   try {
-    if (Platform.OS === "android" && !(await hasAndroidPermission())) {
-      return 'required_permission';
+    if (Platform.OS === 'android') {
+      await checkAndroidPermission();
     }
-
-    console.log(flp);
 
     // CameraRoll.save(flp, { type, album })
     await CameraRoll.save(flp)
@@ -58,3 +19,13 @@ export async function SaveToGalleryAsync(flp: string) {
     return error;
   }
 };
+
+const checkAndroidPermission = async () => {
+  try {
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+    await PermissionsAndroid.request(permission);
+    Promise.resolve();
+  } catch (error) {
+    Promise.reject(error);
+  }
+}
