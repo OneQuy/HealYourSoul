221be0f168def8ac017c376150a07781ab99093f
MinhQuyImobility <quynm@imsoft.vn>
2023-08-31 10:45:43 +0700

    hhe

--- editor/src/Utils_NodeJS.js
+++ editor/src/Utils_NodeJS.js
@@ -1,14 +1,18 @@
 // https://en.m.wikipedia.org/wiki/ANSI_escape_code#Colors:~:text=Eclipse%20Terminal-,30,-40
 function LogRed(...msg) {
-    console.log(`\x1b[31m ${msg.join(', ')} \x1b[0m`);
+    const code = process.platform === 'win32' ? 91 : 31
+
+    console.log(`\x1b[${code}m ${msg.join(', ')} \x1b[0m`);
 }
 
 function LogGreen(...msg) {
-    console.log(`\x1b[32m ${msg.join(', ')} \x1b[0m`);
+    const code = process.platform === 'win32' ? 92 : 32
+    console.log(`\x1b[${code}m ${msg.join(', ')} \x1b[0m`);
 }
 
 function LogYellow(...msg) {
-    console.log(`\x1b[33m ${msg.join(', ')} \x1b[0m`);
+    const code = process.platform === 'win32' ? 93 : 33
+    console.log(`\x1b[${code}m ${msg.join(', ')} \x1b[0m`);
 }
 
 module.exports = {
