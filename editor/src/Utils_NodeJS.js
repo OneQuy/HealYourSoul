8d5a8b1a6583558ce50ef2ace5935ce3f3968cce
Minh Quy <onequy@gmail.com>
2023-08-31 08:22:38 +0700

    log color

--- editor/src/Utils_NodeJS.js
+++ editor/src/Utils_NodeJS.js
@@ -1,8 +1,18 @@
 // https://en.m.wikipedia.org/wiki/ANSI_escape_code#Colors:~:text=Eclipse%20Terminal-,30,-40
-function LogRed(msg) {
-    console.log(`\x1b[31m ${msg} \x1b[0m`);
+function LogRed(...msg) {
+    console.log(`\x1b[31m ${msg.join(', ')} \x1b[0m`);
+}
+
+function LogGreen(...msg) {
+    console.log(`\x1b[32m ${msg.join(', ')} \x1b[0m`);
+}
+
+function LogYellow(...msg) {
+    console.log(`\x1b[33m ${msg.join(', ')} \x1b[0m`);
 }
 
 module.exports = {
-    LogRed
+    LogRed,
+    LogGreen,
+    LogYellow,
 }
