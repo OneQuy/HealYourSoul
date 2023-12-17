90a7c853159a5c289b42ea850e0224acf97567a9
Minh Quy <onequy@gmail.com>
2023-12-17 20:21:45 +0700

    credit

--- scr/screens/IAP/IAPPage_Subscribed.tsx
+++ scr/screens/IAP/IAPPage_Subscribed.tsx
@@ -1,12 +1,10 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Linking } from 'react-native'
 import React, { } from 'react'
 import { FontSize, FontWeight, LocalText, Outline } from '../../constants/AppConstants'
 import { SubscribedData } from '../../constants/Types'
 import { SplitNumberInText } from '../../handle/UtilsTS'
 
 const imgBg = require('../../../assets/images/subscribed-bg.jpg')
-// Photo by Amy Vann on Unsplash
-// https://unsplash.com/photos/a-view-of-a-beach-with-a-boat-in-the-distance-GFsc977iFL4
 
 const IAPPage_Subscribed = ({ subscribedData }: { subscribedData: SubscribedData }) => {
     const monthNum = SplitNumberInText(subscribedData.id)
@@ -19,24 +17,29 @@ const IAPPage_Subscribed = ({ subscribedData }: { subscribedData: SubscribedData
     const dayLeft = Math.ceil((expiredDate.valueOf() - Date.now()) / 1000 / 3600 / 24)
 
     return (
-        <ImageBackground source={imgBg} style={{ flex: 1, gap: Outline.GapVertical, padding: Outline.Horizontal, backgroundColor: 'black' }}>
-            <View style={{ flexDirection: 'row', }}>
-                <Text style={{ color: 'black', fontSize: FontSize.Normal }}>{LocalText.you_subscribed} </Text>
-                <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{monthNum} Month </Text>
+        <ImageBackground source={imgBg} style={{ flex: 1, padding: Outline.Horizontal, justifyContent: 'space-between' }}>
+            <View style={{ gap: Outline.GapVertical, }}>
+                <View style={{ flexDirection: 'row', }}>
+                    <Text style={{ color: 'black', fontSize: FontSize.Normal }}>{LocalText.you_subscribed} </Text>
+                    <View style={{ backgroundColor: 'gold' }}>
+                        <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{monthNum} Month </Text>
+                    </View>
+                </View>
+                <View style={{ flexDirection: 'row', }}>
+                    <Text style={{ color: 'black', fontSize: FontSize.Normal }}>{LocalText.subscribed_date} </Text>
+                    <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{subDate.toLocaleDateString()}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', }}>
+                    <Text style={{ color: 'black', fontSize: FontSize.Normal }}>{LocalText.subscribed_exp_date} </Text>
+                    <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{expiredDate.toLocaleDateString()}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', }}>
+                    <Text style={{ color: 'black', fontSize: FontSize.Normal }}>{LocalText.day_left} </Text>
+                    <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{dayLeft}</Text>
+                </View>
+                <Text style={{ marginTop: Outline.GapVertical_2 * 2, color: 'black', fontStyle: 'italic', fontSize: FontSize.Normal }}>{LocalText.thank_you}</Text>
             </View>
-            <View style={{ flexDirection: 'row', }}>
-                <Text style={{ color: 'black', fontSize: FontSize.Normal }}>Subscribed date: </Text>
-                <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{subDate.toLocaleDateString()}</Text>
-            </View>
-            <View style={{ flexDirection: 'row', }}>
-                <Text style={{ color: 'black', fontSize: FontSize.Normal }}>Expired data: </Text>
-                <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{expiredDate.toLocaleDateString()}</Text>
-            </View>
-            <View style={{ flexDirection: 'row', }}>
-                <Text style={{ color: 'black', fontSize: FontSize.Normal }}>Day left: </Text>
-                <Text style={{ color: 'black', fontWeight: FontWeight.Bold, fontSize: FontSize.Normal }}>{dayLeft}</Text>
-            </View>
-            <Text style={{ marginTop: Outline.GapVertical_2 * 2, color: 'black', fontSize: FontSize.Normal }}>{'I\'m glad that you supported me,' + '\n' + 'Thank you so much!'}</Text>
+            <Text onPress={() => Linking.openURL('https://unsplash.com/photos/a-view-of-a-beach-with-a-boat-in-the-distance-GFsc977iFL4')} style={{ color: 'gray', fontSize: FontSize.Small }}>Photo by Amy Vann on Unsplash.</Text>
         </ImageBackground>
     )
 }
