982ede328b341de560d591f6337ba536eb368a66
Minh Quy <onequy@gmail.com>
2023-12-25 19:58:04 +0700

    random

--- scr/handle/services/Unsplash.ts
+++ scr/handle/services/Unsplash.ts
@@ -1,32 +1,50 @@
 // https://unsplash.com/documentation#get-a-random-photo
 
-import { UNSPLASH_KEY } from '../../../keys'
+import { Dimensions } from "react-native"
+
+// import { UNSPLASH_KEY } from '../../../keys'
 
 // export const GetRandomUnsplashPictureAsync = async (): Promise<string | undefined> => {
 //     try {
 //         const data = await fetch(`https://api.unsplash.com/photos/random?client_id=${UNSPLASH_KEY}`)
-        
+
 //         if (data.status !== 200)
 //             return undefined
 
 //         const dataJ = await data.json();
-    
+
 //         return dataJ.urls.regular
 //     } catch (error) {
 //         return undefined
 //     }
 // }
 
+const screen = Dimensions.get('screen')
 
 export const GetRandomUnsplashPictureAsync = async (): Promise<string | undefined> => {
     try {
-        const w = 1000 + Math.ceil(Math.random() * 500)
-        const h = 1000 + Math.ceil(Math.random() * 500)
+        const isLandscape = Math.random() > 0.5
+
+        let w, h = 0
+
+        const ratio = 1 + Math.random() * (18 / 9 - 1)
+
+        if (isLandscape) {
+            w = screen.width * screen.scale - 500 + Math.ceil(Math.random() * 500)
+            h = w / ratio
+        }
+        else {
+            h = screen.height * screen.scale - 500 + Math.ceil(Math.random() * 500)
+            w = h / ratio
+        }
+
+        h = Math.ceil(h)
+        w = Math.ceil(w)
 
         const data = await fetch(`https://picsum.photos/${w}/${h}`)
-        
+
         // console.log(data);
-        
+
         if (data.status !== 200)
             return undefined
 
