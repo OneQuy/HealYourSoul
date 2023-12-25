01e57ec8133325bd407d48edb90f63d076326b2d
Minh Quy <onequy@gmail.com>
2023-12-25 19:18:26 +0700

    rm log

--- scr/handle/services/Unsplash.ts
+++ scr/handle/services/Unsplash.ts
@@ -5,7 +5,7 @@ import { UNSPLASH_KEY } from '../../../keys'
 export const GetRandomUnsplashPictureAsync = async (): Promise<string | undefined> => {
     try {
         const data = await fetch(`https://api.unsplash.com/photos/random?client_id=${UNSPLASH_KEY}`)
-        console.log(data.headers);
+        // console.log(data.headers);
         
         if (data.status !== 200)
             return undefined
