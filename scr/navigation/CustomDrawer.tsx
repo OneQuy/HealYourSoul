cdf32ede7962c2d14f3861e5accb07e876357405
Minh Quy <onequy@gmail.com>
2023-12-28 18:13:43 +0700

    item couple

--- scr/navigation/CustomDrawer.tsx
+++ scr/navigation/CustomDrawer.tsx
@@ -1,11 +1,10 @@
 import { useRef } from "react";
 import { RootState, useAppDispatch, useAppSelector } from "../redux/Store";
 import { ThemeType, themes } from "../constants/Colors";
-import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList, } from '@react-navigation/drawer';
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { DrawerContentComponentProps, DrawerContentScrollView, } from '@react-navigation/drawer';
+import { Text, TouchableOpacity, View } from "react-native";
 import { setTheme } from "../redux/MiscSlice";
-import { PickRandomElement } from "../handle/Utils";
-import { ColorNameToRgb, RandomColor } from "../handle/UtilsTS";
+import DrawerCoupleItem from "./DrawerCoupleItem";
 
 export function CustomDrawerContent(props: DrawerContentComponentProps) {
   const themeValues = useRef(Object.keys(themes));
@@ -19,11 +18,7 @@ export function CustomDrawerContent(props: DrawerContentComponentProps) {
       <DrawerContentScrollView {...props}>
         {
           arr.map((num) => {
-            return (
-              <View style={{ backgroundColor: RandomColor(), height: 50, width: '100%' }}>
-
-              </View>
-            )
+            return <DrawerCoupleItem key={num} />
           })
         }
       </DrawerContentScrollView>
---
+++ scr/navigation/DrawerCoupleItem.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { RandomColor } from '../handle/UtilsTS'
+
+const DrawerCoupleItem = () => {
+    return (
+        <View style={{ backgroundColor: RandomColor(), height: 50, width: '100%' }}>
+
+        </View>
+    )
+}
+
+export default DrawerCoupleItem
