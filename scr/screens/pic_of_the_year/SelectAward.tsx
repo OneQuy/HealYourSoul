ab3f2c3833baef82ea20703612a7ab8fe6ca4f11
Minh Quy <onequy@gmail.com>
2023-12-31 22:41:54 +0700

    style

--- scr/screens/pic_of_the_year/SelectAward.tsx
+++ scr/screens/pic_of_the_year/SelectAward.tsx
@@ -1,9 +1,12 @@
+// @ts-ignore
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native'
 import React, { useCallback, useContext, useEffect, useMemo, useRef } from 'react'
 import { ThemeContext } from '../../constants/Colors';
 import { CommonStyles } from '../../constants/CommonConstants';
 import { ColorNameToRgb } from '../../handle/UtilsTS';
-import { BorderRadius, FontSize, FontWeight, LocalText, Outline, Size } from '../../constants/AppConstants';
+import { BorderRadius, FontSize, FontWeight, Icon, LocalText, Outline, Size } from '../../constants/AppConstants';
 import { dataOfYears } from './PicturesOfTheYearScreen';
 import { AwardPicture } from '../../constants/Types';
 
@@ -43,7 +46,13 @@ const SelectAward = ({ year, selectIdx, setIdx }: { year: number, selectIdx: num
     return (
         <View style={[styleSheet.masterView, CommonStyles.justifyContentCenter_AlignItemsCenter]}>
             <View style={[{ backgroundColor: theme.background, }, styleSheet.bgView]}>
-                <Text style={styleSheet.title}>{year + ' ' + LocalText.winners}</Text>
+                <View style={[{ flexDirection: 'row' }, CommonStyles.justifyContentCenter_AlignItemsCenter]}>
+                    <MaterialCommunityIcons name={Icon.ThreeDots} color={theme.background} size={Size.Icon} />
+                    <Text style={styleSheet.title}>{year + ' ' + LocalText.winners}</Text>
+                    <TouchableOpacity onPress={() => setIdx(selectIdx)}>
+                        <MaterialCommunityIcons name={Icon.X} color={theme.text} size={Size.Icon} />
+                    </TouchableOpacity>
+                </View>
                 <FlatList
                     // @ts-ignore
                     ref={flatlistRef}
@@ -64,10 +73,10 @@ export default SelectAward
 
 const styleSheet = StyleSheet.create({
     masterView: { backgroundColor: ColorNameToRgb('black', 0.8), width: '100%', height: '100%', position: 'absolute' },
-    bgView: { padding: Outline.GapVertical, width: '80%', height: '70%', borderRadius: BorderRadius.BR },
+    bgView: { gap: Outline.GapVertical, padding: Outline.GapVertical, width: '80%', height: '70%', borderRadius: BorderRadius.BR },
     itemTO: { flexDirection: 'row', alignItems: 'center', gap: Outline.GapHorizontal },
     image: { width: listPopupIconSize, height: listPopupIconSize, },
     flatlist: { gap: listPopupGap },
     text: { fontWeight: FontWeight.B500, fontSize: FontSize.Small_L, flex: 1 },
-    title: { textAlign: 'center', fontWeight: FontWeight.B600, fontSize: FontSize.Big },
+    title: { flex: 1, textAlign: 'center', fontWeight: FontWeight.B600, fontSize: FontSize.Big },
 })
