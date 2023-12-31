9c4f3538a3d6414440e84b51c4cd2957feea679b
Minh Quy <onequy@gmail.com>
2023-12-31 22:35:46 +0700

    const

--- scr/screens/pic_of_the_year/SelectAward.tsx
+++ scr/screens/pic_of_the_year/SelectAward.tsx
@@ -1,11 +1,14 @@
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image, FlatListProps } from 'react-native'
-import React, { LegacyRef, useCallback, useContext, useEffect, useMemo, useRef } from 'react'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native'
+import React, { useCallback, useContext, useEffect, useMemo, useRef } from 'react'
 import { ThemeContext } from '../../constants/Colors';
 import { CommonStyles } from '../../constants/CommonConstants';
 import { ColorNameToRgb } from '../../handle/UtilsTS';
-import { BorderRadius, FontSize, FontWeight, Icon, LocalText, Outline, Size } from '../../constants/AppConstants';
+import { BorderRadius, FontSize, FontWeight, LocalText, Outline, Size } from '../../constants/AppConstants';
 import { dataOfYears } from './PicturesOfTheYearScreen';
 import { AwardPicture } from '../../constants/Types';
+
+const listPopupIconSize = Size.IconBig
+const listPopupGap = Outline.GapVertical
 
 const SelectAward = ({ year, selectIdx, setIdx }: { year: number, selectIdx: number, setIdx: (idx: number) => void }) => {
     const theme = useContext(ThemeContext);
@@ -48,8 +51,8 @@ const SelectAward = ({ year, selectIdx, setIdx }: { year: number, selectIdx: num
                     keyExtractor={(item) => item.title + item.author}
                     contentContainerStyle={styleSheet.flatlist}
                     renderItem={renderItem}
-                    getItemLayout={(data, index) => {
-                        return { length: Size.IconBig, offset: Size.IconBig * index + Outline.GapVertical * (index) - Size.IconBig * 3, index }
+                    getItemLayout={(_, index) => {
+                        return { length: listPopupIconSize, offset: listPopupIconSize * index + listPopupGap * (index) - listPopupIconSize * 3, index }
                     }}
                 />
             </View>
@@ -63,8 +66,8 @@ const styleSheet = StyleSheet.create({
     masterView: { backgroundColor: ColorNameToRgb('black', 0.8), width: '100%', height: '100%', position: 'absolute' },
     bgView: { padding: Outline.GapVertical, width: '80%', height: '70%', borderRadius: BorderRadius.BR },
     itemTO: { flexDirection: 'row', alignItems: 'center', gap: Outline.GapHorizontal },
-    image: { width: Size.IconBig, height: Size.IconBig, },
-    flatlist: { gap: Outline.GapVertical },
+    image: { width: listPopupIconSize, height: listPopupIconSize, },
+    flatlist: { gap: listPopupGap },
     text: { fontWeight: FontWeight.B500, fontSize: FontSize.Small_L, flex: 1 },
     title: { textAlign: 'center', fontWeight: FontWeight.B600, fontSize: FontSize.Big },
 })
