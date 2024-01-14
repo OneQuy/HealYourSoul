3d35921c16c27232e0b01c2eed141388f7126de0
Minh Quy <onequy@gmail.com>
2024-01-14 20:30:11 +0700

    setting

--- scr/screens/setting/SettingScreen.tsx
+++ scr/screens/setting/SettingScreen.tsx
@@ -1,11 +1,32 @@
-import { ScrollView } from 'react-native'
-import React, {  } from 'react'
-import { Outline } from '../../constants/AppConstants'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useContext, useMemo } from 'react'
+import { BorderRadius, FontSize, Outline } from '../../constants/AppConstants'
+import { ThemeContext } from '../../constants/Colors';
+import { CommonStyles } from '../../constants/CommonConstants';
 
 const SettingScreen = () => {
+  const theme = useContext(ThemeContext);
+
+  const style = useMemo(() => {
+    return StyleSheet.create({
+      masterView: { backgroundColor: theme.background, padding: Outline.Horizontal, gap: Outline.GapVertical_2 },
+      topButtonContainerView: { gap: Outline.GapHorizontal, flexDirection: 'row' },
+      topButtonTO: { padding: Outline.GapVertical, borderRadius: BorderRadius.BR8, flex: 1, backgroundColor: theme.primary, justifyContent: 'center', alignItems: 'center' },
+      topButtonText: { color: theme.text, fontSize: FontSize.Small },
+    })
+  }, [theme])
+
   return (
-    <ScrollView contentContainerStyle={{ backgroundColor: 'white', padding: Outline.Horizontal, gap: Outline.GapVertical_2 }}>
-    </ScrollView>
+    <View style={style.masterView}>
+      <View style={style.topButtonContainerView}>
+        <TouchableOpacity style={style.topButtonTO}>
+          <Text style={style.topButtonText}>Setting</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={style.topButtonTO}>
+          <Text style={style.topButtonText}>Setting</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   )
 }
 
