a8aa6f8871a3baf5c5100c0f39160c830fa6116e
Minh Quy <onequy@gmail.com>
2024-01-17 08:00:06 +0700

    fast joke

--- scr/handle/services/NinjaJoke.ts
+++ scr/handle/services/NinjaJoke.ts
@@ -1,29 +1,39 @@
 import axios from 'axios'
 import { NINJA_JOKE_KEY } from '../../../keys';
+import { GetApiDataItemFromCached } from '../AppUtils';
+import { StorageKey_NinjaJoke } from '../../constants/AppConstants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const options = {
     method: 'GET',
     url: 'https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes',
+    params: { limit: '30' },
     headers: {
-        'X-RapidAPI-Key': NINJA_JOKE_KEY ,
+        'X-RapidAPI-Key': NINJA_JOKE_KEY,
         'X-RapidAPI-Host': 'dad-jokes-by-api-ninjas.p.rapidapi.com'
     }
 };
 
 export const GetNinjaJokeAsync = async (): Promise<string | undefined> => {
     try {
+        const cachedText = await GetApiDataItemFromCached<string>(StorageKey_NinjaJoke)
+
+        if (cachedText !== undefined)
+            return cachedText
+
         const response = await axios.request(options);
-        // console.log(response.data);
 
         if (response.status !== 200)
             return undefined
 
-        const txt = response.data[0].joke
-
-        if (typeof txt === 'string')
-            return txt
-        else
+        if (!Array.isArray(response.data) || response.data.length <= 0)
             return undefined
+
+        const textArr = response.data.map(i => i.joke as string)
+
+        await AsyncStorage.setItem(StorageKey_NinjaJoke, JSON.stringify(textArr))
+
+        return await GetApiDataItemFromCached<string>(StorageKey_NinjaJoke)
     } catch (error) {
         return undefined
     }
