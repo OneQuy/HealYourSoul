f3fdfdf1046466d23de3234d97cc6aa1a2eba51c
Minh Quy <onequy@gmail.com>
2024-01-31 08:41:43 +0700

    fix align

--- scr/screens/best_short_films/BestShortFilmsScreen.tsx
+++ scr/screens/best_short_films/BestShortFilmsScreen.tsx
@@ -415,5 +415,5 @@ const styleSheet = StyleSheet.create({
     nameText: { marginLeft: widthPercentageToDP(12), flex: 1, textAlign: 'center', fontSize: FontSize.Normal },
     nameContainerView: { paddingHorizontal: Outline.GapVertical, flexDirection: 'row', gap: Outline.GapHorizontal },
     showListIconView: { padding: Outline.GapHorizontal, borderWidth: StyleSheet.hairlineWidth, borderRadius: BorderRadius.BR8 },
-    openYtb: { marginLeft: Outline.VerticalMini, gap: Outline.GapHorizontal, borderRadius: BorderRadius.BR8, flexDirection: 'row' },
+    openYtb: { alignItems: 'center', marginLeft: Outline.VerticalMini, gap: Outline.GapHorizontal, borderRadius: BorderRadius.BR8, flexDirection: 'row' },
 })
