96009ff8f0507b6e779dcf2007e9f6673a14af06
Minh Quy <onequy@gmail.com>
2024-02-01 18:07:24 +0700

    hehe

--- scr/handle/tracking/TelemetryDeck.ts
+++ scr/handle/tracking/TelemetryDeck.ts
@@ -1,10 +1,11 @@
-import {
-    TelemetryDeckProvider,
-    createTelemetryDeck,
-} from "@typedigital/telemetrydeck-react";
+import { createTelemetryDeck } from "@typedigital/telemetrydeck-react";
 import { IsDev } from "../IsDev";
 import { TELEMETRY_DECK_KEY } from "../../../keys";
 
 console.log('key tele is dev: ' + IsDev());
 
-const telemetryDeck = createTelemetryDeck({ appID: TELEMETRY_DECK_KEY, clientUser: "anonymous", testMode: IsDev() });
+const telemetryDeck = createTelemetryDeck({
+    appID: TELEMETRY_DECK_KEY,
+    clientUser: "anonymous",
+    testMode: IsDev(),
+});
