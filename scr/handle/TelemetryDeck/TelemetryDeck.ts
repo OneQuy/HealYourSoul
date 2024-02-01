a7f3a6d211697810cc2610f190a55007268fb566
Minh Quy <onequy@gmail.com>
2024-02-01 22:00:55 +0700

    rm

--- scr/handle/TelemetryDeck/TelemetryDeck.ts
+++ scr/handle/TelemetryDeck/TelemetryDeck.ts
@@ -13,8 +13,6 @@ import { createTelemetryDeck } from "@typedigital/telemetrydeck-react";
 import { IsDev } from "../IsDev";
 import { TELEMETRY_DECK_KEY } from "../../../keys";
 
-console.log('key tele is dev: ' + IsDev());
-
 export const telemetryDeck = createTelemetryDeck({
     appID: TELEMETRY_DECK_KEY,
     clientUser: "anonymous",
