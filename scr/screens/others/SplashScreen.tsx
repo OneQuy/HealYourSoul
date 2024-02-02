17855de030e46a3d022cf98a091d6a2c7da73fb3
Minh Quy <onequy@gmail.com>
2024-02-02 19:24:43 +0700

    fix status bar at splash screen

--- scr/screens/others/SplashScreen.tsx
+++ scr/screens/others/SplashScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StatusBar } from 'react-native'
 import React from 'react'
 import { FontSize, FontWeight, Outline } from '../../constants/AppConstants';
 import { ThemeColor } from '../../constants/Colors';
@@ -8,6 +8,7 @@ export const logoScr = require('../../../assets/images/logo.png');
 const SplashScreen = ({ theme }: { theme: ThemeColor }) => {
     return (
         <View style={{ backgroundColor: theme.background, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <StatusBar backgroundColor={theme.background} barStyle={theme.shouldStatusBarLight ? 'light-content' : 'dark-content'} />
             <Image source={logoScr} style={{ width: 100, height: 100 }} />
             <Text style={{ color: theme.counterBackground, fontWeight: FontWeight.Bold, marginTop: Outline.GapVertical, fontSize: FontSize.Big }}>Gooday</Text>
             <Text style={{ color: theme.counterBackground, }}>Make your day good</Text>
