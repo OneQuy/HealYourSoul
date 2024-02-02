8b126936dcba4ee76d2e140dc3a83eb643cf290f
Minh Quy <onequy@gmail.com>
2024-02-02 20:42:54 +0700

    update splashscreen

--- scr/screens/others/SplashScreen.tsx
+++ scr/screens/others/SplashScreen.tsx
@@ -2,16 +2,19 @@ import { View, Text, Image, StatusBar } from 'react-native'
 import React from 'react'
 import { FontSize, FontWeight, Outline } from '../../constants/AppConstants';
 import { ThemeColor } from '../../constants/Colors';
+import { heightPercentageToDP } from 'react-native-responsive-screen';
 
 export const logoScr = require('../../../assets/images/logo.png');
+
+const logoSize = heightPercentageToDP(15)
 
 const SplashScreen = ({ theme }: { theme: ThemeColor }) => {
     return (
         <View style={{ backgroundColor: theme.background, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <StatusBar backgroundColor={theme.background} barStyle={theme.shouldStatusBarLight ? 'light-content' : 'dark-content'} />
-            <Image source={logoScr} style={{ width: 100, height: 100 }} />
+            <Image source={logoScr} resizeMode='contain' style={{ height: logoSize, aspectRatio: 1 }} />
             <Text style={{ color: theme.counterBackground, fontWeight: FontWeight.Bold, marginTop: Outline.GapVertical, fontSize: FontSize.Big }}>Gooday</Text>
-            <Text style={{ color: theme.counterBackground, }}>Make your day good</Text>
+            <Text style={{ color: theme.counterBackground, fontSize: FontSize.Small_L }}>Make your day good</Text>
         </View>
     )
 }
