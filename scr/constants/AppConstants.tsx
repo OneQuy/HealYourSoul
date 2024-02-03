93018c87c81728b852f0abfe3b6b168fb346bca0
Minh Quy <onequy@gmail.com>
2024-02-03 11:02:24 +0700

    onboarding screen

--- scr/constants/AppConstants.tsx
+++ scr/constants/AppConstants.tsx
@@ -326,6 +326,13 @@ export const LocalText = {
   multi_choice: 'Multi-choice',
   true_false: 'True/False',
 
+  start_gooday: 'Start to good your day!',
+  color: 'Color',
+  welcome_text: `Welcome to Gooday!`,
+  welcome_text_2: `Your daily dose of laughter and positivity!`,
+  welcome_text_3: `In Gooday, I believe in the power of laughter, positive and beautiful stuffs to brighten your day and lift your spirits. Dive into a world filled with hilarious memes, gorgeous pictures and heartwarming content that is sure to bring a smile to your face.`,
+  set_your_theme: 'Pick your own theme',
+  
   // popup
 
   popup_title_need_internet: 'No Internet',
---
+++ scr/screens/onboarding/Onboarding.tsx
@@ -0,0 +1,107 @@
+// @ts-ignore
+
+import { View, Text, Image, StyleSheet, StatusBar, SafeAreaView, TouchableOpacity } from 'react-native'
+import React, { useCallback, useContext, useMemo, useState } from 'react'
+import { ThemeContext } from '../../constants/Colors';
+import { logoScr } from '../others/SplashScreen';
+import { BorderRadius, FontSize, FontWeight, LocalText, Outline } from '../../constants/AppConstants';
+import { heightPercentageToDP } from 'react-native-responsive-screen';
+import { IsDev } from '../../handle/IsDev';
+import ThemeScroll from '../components/ThemeScroll';
+import { useAppDispatch } from '../../redux/Store';
+import { setTheme } from '../../redux/MiscSlice';
+
+
+const Onboarding = () => {
+    const theme = useContext(ThemeContext);
+    const [showThemes, setShowThemes] = useState(false)
+    const dispatch = useAppDispatch();
+
+    const style = useMemo(() => {
+        return StyleSheet.create({
+            masterView: { flex: 1, backgroundColor: theme.background, justifyContent: 'space-evenly', alignItems: 'center', },
+            logoAppNameContainerView: { gap: Outline.GapVertical, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' },
+            logoImg: { width: heightPercentageToDP(8), aspectRatio: 1 },
+            appNameText: { color: theme.counterBackground, fontSize: FontSize.Big, fontWeight: FontWeight.Bold },
+
+            welcomeView: { alignItems: 'center', },
+            welcomeText: { color: theme.counterBackground, fontSize: FontSize.Big, fontWeight: FontWeight.Bold },
+            welcomeText_2: { color: theme.counterBackground, fontSize: FontSize.Small_L, },
+            welcomeText_3: { textAlign: 'center', margin: Outline.Horizontal, color: theme.counterBackground, fontSize: FontSize.Small_L, },
+
+            bottomBtnsContainerView: { width: '100%', gap: Outline.GapHorizontal, },
+            bottomContainerView: { alignItems: 'center', gap: Outline.GapHorizontal, },
+            themeBtnsContainerView: { flexDirection: 'row', paddingHorizontal: Outline.Horizontal, gap: Outline.GapHorizontal, },
+            startTO: { marginTop: Outline.Vertical, backgroundColor: theme.primary, marginHorizontal: Outline.Horizontal, padding: Outline.Horizontal, borderRadius: BorderRadius.BR, alignItems: 'center' },
+            themeTO: { flex: 1, padding: Outline.GapVertical, borderWidth: StyleSheet.hairlineWidth, borderColor: theme.counterBackground, borderRadius: BorderRadius.BR, alignItems: 'center', justifyContent: 'center', },
+            themeTOText: { color: theme.counterBackground, fontSize: FontSize.Small_L, },
+            btnText: { color: theme.counterPrimary, fontSize: FontSize.Small_L, fontWeight: FontWeight.Bold },
+        })
+    }, [theme])
+
+    const onPressed_Theme = useCallback((type: 'light' | 'dark' | 'other') => {
+        if (type === 'other')
+            setShowThemes(val => !val)
+        else if (type === 'light') {
+            dispatch(setTheme('default_light'))
+        }
+        else if (type === 'dark') {
+            dispatch(setTheme('default_dark'))
+        }
+    }, [])
+
+    return (
+        <SafeAreaView style={style.masterView}>
+            <StatusBar backgroundColor={theme.background} barStyle={theme.shouldStatusBarLight ? 'light-content' : 'dark-content'} />
+
+            {/* logo & app name */}
+            <View style={style.logoAppNameContainerView}>
+                <Image
+                    source={logoScr}
+                    resizeMode='contain'
+                    style={style.logoImg} />
+                <Text style={style.appNameText}>Gooday{IsDev() ? '.' : ''}</Text>
+            </View>
+
+            {/* welcome text */}
+
+            <View style={style.welcomeView}>
+                <Text adjustsFontSizeToFit numberOfLines={1} style={style.welcomeText}>{LocalText.welcome_text}</Text>
+                <Text adjustsFontSizeToFit numberOfLines={1} style={style.welcomeText_2}>{LocalText.welcome_text_2}</Text>
+                <Text style={style.welcomeText_3}>{LocalText.welcome_text_3}</Text>
+            </View>
+
+            {/* bottom */}
+
+            <View style={style.bottomBtnsContainerView}>
+                {/* set theme */}
+                <View style={style.bottomContainerView}>
+                    <Text adjustsFontSizeToFit numberOfLines={1} style={style.welcomeText_2}>{LocalText.set_your_theme}:</Text>
+                    <View style={style.themeBtnsContainerView}>
+                        <TouchableOpacity onPress={() => onPressed_Theme('light')} style={style.themeTO}>
+                            <Text adjustsFontSizeToFit numberOfLines={1} style={style.themeTOText}>{LocalText.lights_mode}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => onPressed_Theme('dark')} style={style.themeTO}>
+                            <Text adjustsFontSizeToFit numberOfLines={1} style={style.themeTOText}>{LocalText.darks_mode}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => onPressed_Theme('other')} style={style.themeTO}>
+                            <Text adjustsFontSizeToFit numberOfLines={1} style={style.themeTOText}>{LocalText.color}</Text>
+                        </TouchableOpacity>
+                    </View>
+                    {
+                        !showThemes ? undefined :
+                            <ThemeScroll mode='all' />
+                    }
+                </View>
+
+                {/* start btn */}
+                <TouchableOpacity style={style.startTO}>
+                    <Text adjustsFontSizeToFit numberOfLines={1} style={style.btnText}>{LocalText.start_gooday}</Text>
+                </TouchableOpacity>
+            </View>
+
+        </SafeAreaView>
+    )
+}
+
+export default Onboarding
