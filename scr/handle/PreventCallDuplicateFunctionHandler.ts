ff8ccdde37eec0c4ba2e9b3368dffa2ac9bbf9a0
Minh Quy <onequy@gmail.com>
2024-02-23 07:42:40 +0700

    ts

--- scr/handle/PreventCallDuplicateFunctionHandler.ts
+++ scr/handle/PreventCallDuplicateFunctionHandler.ts
@@ -2,16 +2,18 @@ import { DelayAsync } from "./Utils"
 
 export class PreventCallDuplicateFunctionHandler<T> {
     private funcAsync: () => Promise<T>
-    private result: T | undefined
     private isHandling: boolean = false
     private checkIntervalMs: number
+    
+    // @ts-ignore
+    private result: T
 
     constructor(funcAsync: () => Promise<T>, checkIntervalMs: number = 1000) {
         this.funcAsync = funcAsync
         this.checkIntervalMs = checkIntervalMs
     }
 
-    async ExcecuteAsync(): Promise<T | undefined> {
+    async ExcecuteAsync(): Promise<T> {
         if (this.isHandling) {
             for (; ;) {
                 // console.log('is handling, wait...');
