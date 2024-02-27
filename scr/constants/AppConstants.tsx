3ca7b72ed7d730e4cfae77fbb41aa008a3236219
Minh Quy <onequy@gmail.com>
2024-02-27 12:07:06 +0700

    fix text

--- scr/constants/AppConstants.tsx
+++ scr/constants/AppConstants.tsx
@@ -337,7 +337,7 @@ export const LocalText = {
   warning_premium: 'These subscriptions are non-renewable. If you no longer wish to subscribe, it will be automatically canceled at the end of the period.',
   thank_you_premium: "Any purchase is greatly appreciated. Thank you 💛",
 
-  diversity_empty_saved: "You have no item.\nSimply tap the Save icon on the header of the post you'd like to watch later, and it will be saved here for your convenience.",
+  diversity_empty_saved: "You have no item.\n\nSimply tap the Save icon on the header of the post you'd like to watch later, and it will be saved here for your convenience.",
   show_all: "Show all",
 
   // ad_free: 'Ad-free',
