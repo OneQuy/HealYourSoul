ae604ae4c2e0ceff71f166f86dd895d8738054c7
Minh Quy <onequy@gmail.com>
2024-02-28 11:23:44 +0700

    wiki

--- scr/handle/services/Wikipedia.ts
+++ scr/handle/services/Wikipedia.ts
@@ -4,7 +4,7 @@ const url = 'https://en.wikipedia.org/api/rest_v1/page/random/summary'
 
 export const GetWikiAsync = async (): Promise<object | undefined> => {
     try {
-        
+
         let res: Response | undefined
 
         for (let i = 0; i < 5; i++) {
@@ -22,9 +22,38 @@ export const GetWikiAsync = async (): Promise<object | undefined> => {
         }
 
         const json = await res.json()
-        return json
+        return extractDataFromRawObject(json)
     } catch (error) {
         console.error(error);
         return undefined
+    }
+}
+
+
+const extractDataFromRawObject = (data: object | undefined) => {
+    if (data === undefined)
+        return undefined
+
+    return {
+        // @ts-ignore
+        extract: data.extract,
+        // @ts-ignore
+        title: data.title,
+
+        thumbnail: {
+            // @ts-ignore
+            source: data.thumbnail?.source
+        },
+
+        content_urls: {
+            mobile: {
+                // @ts-ignore
+                page: data.content_urls?.mobile?.page
+            },
+            desktop: {
+                // @ts-ignore
+                page: data.content_urls?.desktop?.page
+            }
+        }
     }
 }
