376f9444e729f2f3614694c2b754ba5c01fb753f
Minh Quy <onequy@gmail.com>
2024-03-02 14:40:12 +0700

    style for send msg screen

--- scr/screens/admin/SendInboxView.tsx
+++ scr/screens/admin/SendInboxView.tsx
@@ -18,33 +18,19 @@ const SendInboxView = () => {
     return StyleSheet.create({
       masterView: { paddingBottom: insets.bottom + Outline.Horizontal, flex: 1, backgroundColor: theme.background, padding: Outline.Horizontal, paddingVertical: Outline.GapHorizontal },
       scrollView: { gap: Outline.GapHorizontal },
-      flexRowWithGap: { flexDirection: 'row', gap: Outline.GapHorizontal },
-      rateContainerView: { flexDirection: 'row', gap: Outline.GapHorizontal, justifyContent: 'center', alignItems: 'center' },
-      checkbox: { flexDirection: 'row', gap: Outline.GapHorizontal, alignItems: 'center', justifyContent: 'space-between' },
+      btnText: { textAlign: 'center', color: theme.counterPrimary, fontSize: FontSize.Small_L, fontWeight: FontWeight.B500 },
       textInputConView: { height: heightPercentageToDP(20), padding: Outline.GapVertical, borderColor: theme.primary, borderRadius: BorderRadius.BR8, borderWidth: StyleSheet.hairlineWidth },
       textInputUserContactConView: { height: heightPercentageToDP(5), paddingHorizontal: Outline.GapVertical, borderColor: theme.primary, borderRadius: BorderRadius.BR8, borderWidth: StyleSheet.hairlineWidth },
-      emailCopyTO: { justifyContent: 'center', alignItems: 'center' },
-      communityIconTO: { paddingHorizontal: Outline.GapVertical, flex: 1, flexDirection: 'row', gap: Outline.GapHorizontal, backgroundColor: theme.primary, paddingVertical: Outline.GapVertical, borderRadius: BorderRadius.BR8, justifyContent: 'center', alignItems: 'center' },
-      communitiyTOTxt: { flex: 1, textAlign: 'center', color: theme.counterPrimary, fontSize: FontSize.Small_L },
       titleText: { color: theme.primary, fontSize: FontSize.Small_L, fontWeight: FontWeight.B600 },
-      litmiFeedbackText: { color: theme.counterBackground, fontSize: FontSize.Small_L, fontWeight: '300' },
-      btnText: { textAlign: 'center', color: theme.counterPrimary, fontSize: FontSize.Small_L, fontWeight: FontWeight.B500 },
       descNotiText: { color: theme.counterBackground, fontSize: FontSize.Small },
       contentTxt: { color: theme.counterBackground, fontSize: FontSize.Small_L },
-      contentTxt_withBold: { color: theme.counterBackground, fontSize: FontSize.Small_L, fontWeight: FontWeight.Bold },
       sendFeedbackTO: { backgroundColor: theme.primary, minWidth: 100, alignSelf: 'center', paddingVertical: Outline.GapVertical, paddingHorizontal: Outline.GapVertical_2, borderRadius: BorderRadius.BR8 },
-      shareTO: { backgroundColor: theme.primary, flexDirection: 'row', justifyContent: 'center', gap: Outline.GapHorizontal, flex: 1, alignSelf: 'center', paddingVertical: Outline.GapVertical, paddingHorizontal: Outline.GapVertical_2, borderRadius: BorderRadius.BR8, },
       sendFeedbackInput: { color: theme.counterBackground, textAlignVertical: 'top', textAlign: 'left', width: '100%', height: '100%' },
-      sendFeedbackInput_UserContact: { color: theme.counterBackground, textAlignVertical: 'center', textAlign: 'left', width: '100%', height: '100%' },
     })
   }, [theme, insets])
 
   const send = useCallback(() => {
   }, [])
-
-  const hair100Width = useCallback(() => {
-    return <View style={{ marginVertical: Outline.Horizontal, backgroundColor: theme.counterBackground, width: '100%', height: StyleSheet.hairlineWidth }} />
-  }, [theme])
 
   useEffect(() => {
     (async () => {
@@ -56,10 +42,23 @@ const SendInboxView = () => {
       <ScrollView
         showsVerticalScrollIndicator={false} contentContainerStyle={style.scrollView}>
 
-        {/* feedback */}
+        {/* title */}
 
-        <Text style={style.titleText}>{LocalText.feedback}</Text>
-        <Text style={style.contentTxt}>{LocalText.feedback_info}</Text>
+        <Text style={style.titleText}>title</Text>
+
+        <View style={style.textInputUserContactConView}>
+          <TextInput
+            style={style.sendFeedbackInput}
+            value={titleText}
+            onChangeText={setTitleText}
+            placeholderTextColor={theme.counterBackground}
+            autoCapitalize={'none'}
+          />
+        </View>
+
+        {/* content */}
+
+        <Text style={style.titleText}>content</Text>
 
         <View style={style.textInputConView}>
           <TextInput
@@ -70,21 +69,77 @@ const SendInboxView = () => {
           />
         </View>
 
-        <View />
-        <View />
+        {/* image uri */}
 
-        <Text style={style.descNotiText}>{LocalText.feedback_user_contact}</Text>
+        <Text style={style.titleText}>image url</Text>
 
         <View style={style.textInputUserContactConView}>
           <TextInput
             style={style.sendFeedbackInput}
             value={titleText}
             onChangeText={setTitleText}
-            placeholder='your_email@gmail.com'
             placeholderTextColor={theme.counterBackground}
             autoCapitalize={'none'}
           />
         </View>
+
+        {/* primaryBtnTxt */}
+
+        <Text style={style.titleText}>primaryBtnTxt</Text>
+
+        <View style={style.textInputUserContactConView}>
+          <TextInput
+            style={style.sendFeedbackInput}
+            value={titleText}
+            onChangeText={setTitleText}
+            placeholderTextColor={theme.counterBackground}
+            autoCapitalize={'none'}
+          />
+        </View>
+
+        {/* primaryBtnUrl */}
+
+        <Text style={style.titleText}>primaryBtnUrl</Text>
+
+        <View style={style.textInputUserContactConView}>
+          <TextInput
+            style={style.sendFeedbackInput}
+            value={titleText}
+            onChangeText={setTitleText}
+            placeholderTextColor={theme.counterBackground}
+            autoCapitalize={'none'}
+          />
+        </View>
+
+        {/* primaryBtnGoToScreen */}
+
+        <Text style={style.titleText}>primaryBtnGoToScreen</Text>
+
+        <View style={style.textInputUserContactConView}>
+          <TextInput
+            style={style.sendFeedbackInput}
+            value={titleText}
+            onChangeText={setTitleText}
+            placeholderTextColor={theme.counterBackground}
+            autoCapitalize={'none'}
+          />
+        </View>
+
+        {/* goToScreenParamObj */}
+
+        <Text style={style.titleText}>goToScreenParamObj</Text>
+
+        <View style={style.textInputUserContactConView}>
+          <TextInput
+            style={style.sendFeedbackInput}
+            value={titleText}
+            onChangeText={setTitleText}
+            placeholderTextColor={theme.counterBackground}
+            autoCapitalize={'none'}
+          />
+        </View>
+
+        {/* send */}
 
         <TouchableOpacity onPress={send} style={style.sendFeedbackTO}>
           {
@@ -93,10 +148,6 @@ const SendInboxView = () => {
               <Text style={style.btnText}>{LocalText.send}</Text>
           }
         </TouchableOpacity>
-        {
-          hair100Width()
-        }
-
       </ScrollView>
     </View >
   )
