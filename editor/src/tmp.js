803b80b813cb1827c939291f3d5d9d1ef3850e12
Minh Quy <onequy@gmail.com>
2024-03-14 12:11:08 +0700

    gen movies quote

--- editor/src/tmp.js
+++ editor/src/tmp.js
@@ -5,9 +5,31 @@ const filepath = './assets/json/tmp.txt'
 
 const Tmp = async () => {
 
-    const res = await fetch('https://www.imdb.com/chart/top/?ref_=nv_mv_250')
+    // const res = await fetch('https://www.imdb.com/chart/top/?ref_=nv_mv_250')
 
-    fs.writeFileSync(filepath, await res.text())
+    // fs.writeFileSync(filepath, await res.text())
+    // LogGreen('done')
+
+    Gen_movie_quotes_raw()
+}
+
+const Gen_movie_quotes_raw = async () => {
+    const source_json = './editor/movie_quotes_raw.json'
+    const destPath = './editor/jsons/movie_quotes.json'
+
+    const text = fs.readFileSync(source_json, 'utf-8')
+
+    const rawarr = JSON.parse(text)
+
+    const arr = []
+
+    rawarr.forEach(element => {
+        arr.push(`${element.quote}\n\n(${element.movie}, ${element.year})`)
+    });
+
+    // console.log(arr);
+
+    fs.writeFileSync(destPath, JSON.stringify(arr, null, 1))
     LogGreen('done')
 }
 
