45955f23024796d78b2e514732e86dce60ccae4b
Minh Quy <onequy@gmail.com>
2024-03-14 08:46:26 +0700

    lol anime images

--- scr/screens/anime/AnimeImage.tsx
+++ scr/screens/anime/AnimeImage.tsx
@@ -1,11 +1,27 @@
 import { useCallback } from 'react';
-import { Category, StorageKey_AnimeImage_CurrentIdx } from '../../constants/AppConstants';
+import { Category, StorageKey_AnimeImage_ByType, StorageKey_AnimeImage_CurrentIdx } from '../../constants/AppConstants';
 import { TheRandomImage_PopupSelect } from '../template/TheRandomImage_PopupSelect';
 import { PopupSelectItem } from '../components/PopupSelect';
 import { RandomImage } from '../../constants/Types';
+import { GetApiDataItemFromCached } from '../../handle/AppUtils';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AnimeImageScreen = () => {
     const getImage = useCallback(async (item: PopupSelectItem): Promise<RandomImage | undefined> => {
+        // try get local first
+
+        let link = await GetApiDataItemFromCached<string>(StorageKey_AnimeImage_ByType(item.displayText))
+
+        if (link !== undefined) {
+            // console.log('caced', link);
+
+            return {
+                uri: link
+            } as RandomImage
+        }
+
+        // fetch
+
         const url = `https://api.waifu.pics/many/sfw/${item.displayText}`
 
         try {
@@ -14,17 +30,27 @@ export const AnimeImageScreen = () => {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ exclude: null}),
+                body: JSON.stringify({ exclude: null }),
             })
 
             if (!res.ok)
                 return undefined
 
-            const json = await res.json()
+            const links = (await res.json()).files as string[]
+            // console.log('fetched', links.length);
 
-            return {
-                uri: json.files[0]
-            } as RandomImage
+            await AsyncStorage.setItem(StorageKey_AnimeImage_ByType(item.displayText), JSON.stringify(links))
+
+            link = await GetApiDataItemFromCached<string>(StorageKey_AnimeImage_ByType(item.displayText))
+            // console.log('aa', link);
+
+            if (link !== undefined) {
+                return {
+                    uri: link
+                } as RandomImage
+            }
+            else
+                return undefined
         }
         catch {
             return undefined
