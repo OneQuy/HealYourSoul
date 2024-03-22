0e05d0b75fd6d6038bdcd339cb13d2b8903123fd
Minh Quy <onequy@gmail.com>
2024-03-22 22:28:13 +0700

    add admob json

--- scr/handle/ads/Admob.ts
+++ scr/handle/ads/Admob.ts
@@ -2,10 +2,15 @@
  * Install: 
  * 1. npm i react-native-google-mobile-ads react-native-tracking-transparency
  * 2. npx pod-install
- * 3. add this to plist file:
+ * 3. add this to app.json:
+        "react-native-google-mobile-ads": {
+            "android_app_id": "ca-app-pub-xxxx",
+            "ios_app_id": "ca-app-pub-xxxxx"
+        }
+ * 4. add this to plist file:
         <key>NSUserTrackingUsageDescription</key>
         <string>This identifier will be used to deliver personalized ads to you.</string>
- * 4. done!
+ * 5. done!
  * 
  * Doc: https://docs.page/invertase/react-native-google-mobile-ads/displaying-ads#rewarded-interstitial-ads
  */
