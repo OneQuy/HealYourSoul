59fd862c064d7acbf167b1cb713c6341353f0c0d
Minh Quy <onequy@gmail.com>
2024-05-18 21:53:33 +0700

    OneQuyApp

---
+++ scr/screens/components/OneQuyApp.tsx
@@ -0,0 +1,143 @@
+import { View, Text, StyleSheet, ColorValue, TouchableOpacity, Dimensions } from 'react-native'
+import React, { useMemo } from 'react'
+import ImageBackgroundWithLoading from './ImageBackgroundWithLoading'
+
+const Window = Dimensions.get('window')
+
+const BorderRadius = Window.height * 0.02
+
+const Padding = Window.height * 0.008
+
+const OneQuyApp = ({
+    primaryColor = '#1c1c1c',
+    counterBackgroundColor = '#C1C1C1',
+    backgroundColor = '#fafafa',
+    fontSize = 13,
+}: {
+    primaryColor?: ColorValue
+    counterBackgroundColor?: ColorValue
+    backgroundColor?: ColorValue,
+    fontSize?: number,
+}) => {
+    const currentApp = useMemo(() => {
+        return {
+            logo: 'https://play-lh.googleusercontent.com/eucsatvqG8yTQSl2k_2kCFHk1OLD1chJGsxL8JXn1gldKKfHEmZX4WrWPpUZk3Xaew=w480-h960-rw'
+        }
+    }, [])
+
+    console.log(backgroundColor);
+    
+    const style = useMemo(() => {
+        return StyleSheet.create({
+            master: {
+                width: '100%',
+                backgroundColor: backgroundColor,
+                gap: Padding,
+            },
+
+            titleView: {
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+            },
+
+            titleTxt: {
+                // padding: Padding,
+                fontSize: fontSize * 1.5,
+                color: primaryColor,
+                fontWeight: 'bold',
+            },
+
+            installTO: {
+                padding: Padding,
+                backgroundColor: primaryColor,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: BorderRadius,
+            },
+
+            installTxt: {
+                fontSize,
+                color: backgroundColor,
+            },
+
+            nextTO: {
+                padding: Padding,
+                width: '20%',
+                backgroundColor: counterBackgroundColor,
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: BorderRadius,
+            },
+
+            nextTxt: {
+                fontSize,
+                color: backgroundColor,
+            },
+
+            descriptionTxt: {
+                fontSize,
+                color: counterBackgroundColor,
+                // textAlign:'justify'
+            },
+
+            descriptionView: {
+                flexDirection: 'row',
+                gap: Window.height * 0.01,
+                justifyContent: 'space-between',
+                alignItems: 'center',
+            },
+
+            logoImg: {
+                height: Window.height * 0.08,
+                aspectRatio: 1,
+                borderRadius: BorderRadius,
+                overflow: 'hidden',
+            },
+
+            descriptionTxtView: {
+                flex: 1,
+                // backgroundColor: '#aaffee',
+            },
+
+        })
+    }, [
+        backgroundColor,
+        counterBackgroundColor,
+        primaryColor,
+        fontSize,
+    ])
+
+    return (
+        <View style={style.master}>
+            {/* title */}
+            <View style={style.titleView}>
+                <Text style={style.titleTxt}>Vocaby</Text>
+                {/* go next btn */}
+                <TouchableOpacity style={style.nextTO}>
+                    <Text style={style.nextTxt}>{'Next'}</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* description */}
+            <View style={style.descriptionView}>
+                {/* logo */}
+                <ImageBackgroundWithLoading
+                    source={{ uri: currentApp.logo }}
+                    style={style.logoImg}
+                />
+                {/* description */}
+                <View style={style.descriptionTxtView}>
+                    <Text style={style.descriptionTxt}>{'Vocaby is your pocket English tutor, delivering vocabulary lessons directly to your mobile device through convenient notifications. Enhance your English skills effortlessly on the go!'}</Text>
+                </View>
+            </View>
+
+
+            <TouchableOpacity style={style.installTO}>
+                <Text style={style.installTxt}>{'Install'}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default OneQuyApp
